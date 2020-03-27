ebe2df09ca4dfbdb6c4306babd98901b922498fb
rejaques <renanjaques@outlook.com>
2020-03-27 11:13:54 -0300

    Primeira View criada (Departamentos)

---
+++ VendasWebMVC/Models/Departamento.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VendasWebMVC.Models
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Departamento()
+        {
+        }
+
+        public Departamento(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
