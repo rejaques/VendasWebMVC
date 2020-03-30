4d98f1f940dff20def4f066d51ab19e10604506f
rejaques <renanjaques@outlook.com>
2020-03-30 18:45:54 -0300

    Adicionada a exception de vendedores que contém vendas

--- VendasWebMVC/Services/VendedorService.cs
+++ VendasWebMVC/Services/VendedorService.cs
@@ -35,9 +35,17 @@ namespace VendasWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Vendedor.FindAsync(id);
-            _context.Vendedor.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Vendedor.FindAsync(id);
+                _context.Vendedor.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
+            
         }
 
         public async Task UpdateAsync(Vendedor obj)
